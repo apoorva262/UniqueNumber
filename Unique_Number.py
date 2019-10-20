e1ba017516c5021f8adb3dbf3c93efc56999dac0
apoorva <43111081+apoorva262@users.noreply.github.com>
2019-10-20 11:02:05 +0530

    Create Unique_Number.py
    Use the code to get all the "prime numbers" or "unique number" between any specific range. 
    - If you call the function UN(2, 2000) it will give you all the prime numbers between 2 to 2000 and to get any prime number you can go as high as you want, for ex. UN(2, 2**999999999999999), this will give you prime number between the given range.
    - If you call the function UN(111 to 2000) this will give you all the unique number between 111 to 2000 and again there is no limit to this.

---
+++ Unique_Number.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+def UN(Y, X):
+    Input = pd.DataFrame({'Num':np.arange(X,Y+1), 'Tracker':('')}).set_index('Num')
+    Prime = []
+    while len(Input.index)>0:
+        Prime.append(Input.index[0]) #this will store all the prime numbers
+        for i in Input.index:
+            if i % Input.index[0] == 0: #this will divide all the Num cell from the first cell.
+                Input.loc[i, 'Tracker'] = '1' #this will update the cell in column Tracker '1' which is divisible by (Input.index[0]) in that run
+            else:
+                Input.loc[i, 'Tracker'] = '0' #this will update the cell in column Tracker '0' which is divisible by (Input.index[0]) in that run
+        Input = (Input.loc[Input['Tracker']=='0']) #this will filter only the one's with '0' and drop '1'. So now Input dataframe has only prime numbers.
+    return Prime
